f258f7a8b5a0c1c0ca8208ff7758f971ba9211a8
asanxvgleb <asanxvgleb@gmail.com>
2025-04-29 11:32:19 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,145 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    private static final String word = "java";
+    private static final char[] maskedWord = new char[word.length()];
+    private static int attempts = 6;
+    public static final String[] hangmanStages = {
+            """
+    --------
+    |      |
+    |      O
+    |     \\|/
+    |      |
+    |     / \\
+    |
+    """,
+            """
+    --------
+    |      |
+    |      O
+    |     \\|/
+    |      |
+    |     /
+    |
+    """,
+            """
+    --------
+    |      |
+    |      O
+    |     \\|/
+    |      |
+    |
+    |
+    """,
+            """
+    --------
+    |      |
+    |      O
+    |     \\|
+    |      |
+    |
+    |
+    """,
+            """
+    --------
+    |      |
+    |      O
+    |      |
+    |      |
+    |
+    |
+    """,
+            """
+    --------
+    |      |
+    |      O
+    |
+    |
+    |
+    |
+    """,
+            """
+    --------
+    |      |
+    |
+    |
+    |
+    |
+    |
+    """
+    };
+
+
+    public static void main(String[] args) {
+        startGameLoop();
+
+    }
+
+    public static void startGameLoop() {
+        makeMaskedWord();
+        while (true) {
+            System.out.println("Enter your letter: ");
+            startGame();
+            if (Arrays.equals(word.toCharArray(), maskedWord)) {
+                System.out.println("You won! Word is " + word);
+                break;
+            }
+            if (attempts == 0) {
+                System.out.println("You lose");
+                break;
+            }
+        }
+    }
+    public static void makeMaskedWord() {
+        Arrays.fill(maskedWord, '*');
+        System.out.println(maskedWord);
+    }
+
+    public static void startGame() {
+        char userChar = readChars();
+        showMaskedWord(userChar);
+        countUsersAttempts(userChar);
+    }
+
+    public static char readChars() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.next().toLowerCase().charAt(0);
+
+    }
+
+    public static boolean checkUserInput(char userChar) {
+        for (int i = 0; i < word.length(); i++) {
+            if (word.charAt(i) == userChar) {
+                System.out.println(userChar + " is in word");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void showMaskedWord(char userChar) {
+        if (checkUserInput(userChar)) {
+            for (int i = 0; i < word.length(); i++) {
+                if (word.charAt(i) == userChar) {
+                    maskedWord[i] = userChar;
+                }
+            }
+            System.out.println(maskedWord);
+            System.out.println("--------------------");
+        }
+    }
+
+    public static int countUsersAttempts(char userChar) {
+        if (!checkUserInput(userChar)) {
+            attempts--;
+            System.out.println(hangmanStages[attempts]);
+            if(attempts==1) System.out.println("You have " + attempts + " attempt");
+            else System.out.println("You have " + attempts + " attempts");
+            System.out.println("----------------");
+        }
+        return attempts;
+    }
+
+}
